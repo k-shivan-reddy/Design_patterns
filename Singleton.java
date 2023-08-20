5d86b66f35571ef291d123310df7d5782657197a
k-shivan-reddy <f2013362h@alumni.bits-pilani.ac.in>
2023-08-20 10:10:35 +0530

    multiple methods of singleton

--- Singleton.java
+++ Singleton.java
@@ -1,10 +1,18 @@
-public class Singleton {
+//Incase Multi thread envirnoment we might end up in two instances
+
+public class Singleton
+{
+// 
 private static Singleton instance = null;
+
+// constructor should be made private
 private Singleton()
 {
 
 }
-static Singleton GetInstance()
+
+//Function used to get single instance
+static Singleton getInstance()
 {
     if(instance == null)
     {
@@ -12,4 +20,76 @@ static Singleton GetInstance()
     }
 return instance;
 }
+
+}
+
+// Possible solution for above problem
+class SingletonEargerInitialisation
+{
+private static SingletonEargerInitialisation instance = new SingletonEargerInitialisation();
+// constructor should be made private
+private SingletonEargerInitialisation()
+{
+
+}
+
+//Function used to get single instance
+static SingletonEargerInitialisation getInstance()
+{
+return instance;
+}
+
+}
+
+
+
+class SingletonSynchronised
+{
+// 
+private static SingletonSynchronised instance = null;
+
+// constructor should be made private
+private SingletonSynchronised()
+{
+
+}
+
+//Function used to get single instance
+static synchronized SingletonSynchronised getInstance()
+{
+    if(instance == null)
+    {
+        instance  = new SingletonSynchronised();
+    }
+return instance;
+}
+
+}
+
+
+class SingletonDoubleLock
+{
+// 
+private static SingletonDoubleLock instance = null;
+
+// constructor should be made private
+private SingletonDoubleLock()
+{
+
+}
+
+//Function used to get single instance
+static  SingletonDoubleLock getInstance()
+{
+    if(instance == null)
+    {
+        synchronized(SingletonDoubleLock.class)
+        {
+            instance  = new SingletonDoubleLock();
+        }
+
+    }
+return instance;
+}
+
 }
